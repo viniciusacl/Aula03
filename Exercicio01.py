f2b3d3d341ca6d2ec2e8289a315917a36ecf1943
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-01 14:01:33 -0300

    Envio do primeiro Exercicio

---
+++ Exercicio01.py
@@ -0,0 +1,5 @@
+nome = input("Insira o seu Nome: ")
+idade = input("Digite a sua Idade: ")
+salario =  input("Informe seu Salario: ")
+
+print(f"Seu nome é {nome}, Você tem {idade}, e recebe {salario}, por mês! ")
