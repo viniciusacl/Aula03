941a66e1ee977996067e6fd433c0fbb949a0739b
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-01 15:57:38 -0300

    Envio do Sexto Exercicio

---
+++ Exercicio06.py
@@ -0,0 +1,11 @@
+N1 = float(input("Digite sua Primeira nota: "))
+N2 = float(input("Digite sua Segunda nota: "))
+N3 = float(input("Digite sua Terceira nota: "))
+
+Media = (N1 + N2 + N3) / 3
+if Media >= 7:
+    print(f"Aprovado {Media}")
+else:
+    print(f"Reprovado {Media}")
+
+    
