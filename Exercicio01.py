d563b7c61827e311839fee8d6b847efa404c6abd
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-01 14:18:51 -0300

    Enviando alterações no meu Exercicio

--- Exercicio01.py
+++ Exercicio01.py
@@ -1,5 +1,13 @@
 nome = input("Insira o seu Nome: ")
 idade = input("Digite a sua Idade: ")
-salario =  input("Informe seu Salario: ")
+salario =  float(input("Informe seu Salario: "))
+
 
 print(f"Seu nome é {nome}, Você tem {idade}, e recebe {salario}, por mês! ")
+
+percentual = float(input("Quantos porcento de aumento você teve: "))
+aumento = salario * (percentual/100)
+aumento = aumento + salario
+
+print(f"{nome}, você teve um aumento de {aumento} R$ ")
+
