edec3d4075af59dad7d0b6192a54f10298e0a06f
Vinicius Lima <vinicius2014fl22@hotmail.com>
2025-04-03 14:27:29 -0300

    Enviando alterações no meu codigo

--- Exercicio08 - Preço combustivel..py
+++ Exercicio08 - Preço combustivel..py
@@ -4,10 +4,14 @@ tipo = input("Digite qual o tipo de combustivel:\n"
 qtd = float(input("Qauntos Litros: "))
 vGas = 5.8
 vEtal = 4.9
+valor = 0
 
-if tipo == "G":
+if tipo == "G" or tipo == "g":
     valor = vGas * qtd
-else:
+elif tipo == "E" or tipo == "e":
     valor = vEtal * qtd
+else:
+    print("Tipo de combustível inválido!")
 
 print(f"Você gastou R${valor:.2f}")
+
